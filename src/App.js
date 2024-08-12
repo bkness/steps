adf40b7c14a819f58c8090386035a81ba8ce95fc
Brandon Kelly <kbrandon863@gmail.com>
2024-08-12 13:10:47 -0700

    Implement toggle functionality with useState and wrap JSX with conditional rendering. Used the component tree in dev tools to manually adjust the hooks.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
+// A piece of state or state variable is created to keep track of the current step. The handlePrevious and handleNext functions are used to navigate between steps.
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  // const [test, setTest] = useState({ name: "Brandon" });
 
   function handlePrevious() {
     if (step - 1) setStep(step - 1);
@@ -15,35 +19,49 @@ export default function App() {
 
   function handleNext() {
     if (step < 3) setStep(step + 1);
+
+    // BAD PRACTICE FOR MUTATING STATE WITH COMPLEX DATA TYPES
+    // test.name = "Fred";
+
+    // Always treat state as immutable
+    // setTest({ name: "Fred" });
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>1</div>
-        <div className={step >= 3 ? "active" : ""}>1</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>1</div>
+            <div className={step >= 3 ? "active" : ""}>1</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-          // onMouseEnter={() => alert("TEST")}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+              // onMouseEnter={() => alert("TEST")}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
